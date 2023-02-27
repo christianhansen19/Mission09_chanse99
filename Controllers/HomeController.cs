c70c0b2c5b7f6f40ec29cf9952723b28d5b382b7
Chris Hansen <chris.hansen1298@gmail.com>
2023-02-27 13:55:46 -0700

    One video left

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Mission09_chanse99.Models;
+using Mission09_chanse99.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,11 +18,26 @@ namespace Mission09_chanse99.Controllers
             repo = temp;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int pageNum = 1)
         {
-            var blah = repo.Books.ToList();
+            int pageSize = 10;
 
-            return View(blah);
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
         }
     }
 }
