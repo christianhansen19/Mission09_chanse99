be9e1538f365af77e398015881c93b39d68be60a
Chris Hansen <chris.hansen1298@gmail.com>
2023-03-06 13:36:33 -0700

    bootstrap adjustments

--- Startup.cs
+++ Startup.cs
@@ -53,6 +53,11 @@ namespace Mission09_chanse99
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "Paging",
+                    pattern: "Page{pageNum}",
+                    defaults: new { Controller = "Home", action = "Index" });
+
                 endpoints.MapDefaultControllerRoute();
             });
         }
