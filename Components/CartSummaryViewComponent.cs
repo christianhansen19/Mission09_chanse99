c5aca748d49a3cad6eeea86e3ac228e5b554360d
Chris Hansen <chris.hansen1298@gmail.com>
2023-03-09 23:02:41 -0700

    Added the Cart Icon with the Summary of the Cart

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_chanse99.Models;
+
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket b)
+        {
+            basket = b;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
